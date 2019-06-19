16b74abd2a5eae73834bff4578d61a787d5c11ef
Kiryl Stasevich <kirylstasevich@gmail.com>
2019-06-19 15:53:47 +0300

    setup eslint/prettier and precommit hook

--- pages/admin/content/edit.jsx
+++ pages/admin/content/edit.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import Button from '@material-ui/core/Button'
-import {Form, Text } from 'informed';
-import {load, save} from '../../../core/client/api/content';
-import {getContentTypeMeta} from '../../../core/entities/content-type';
+import { load, save } from '../../../core/client/api/content';
+import { getContentTypeMeta } from '../../../core/entities/content-type';
 import ContentTypeForm from '../../../core/ui/ContentTypeForm';
 
-const Edit = ({content, typeSchema}) => (
-  <ContentTypeForm 
-    initialValues={content} 
+const Edit = ({ content, typeSchema }) => (
+  <ContentTypeForm
+    initialValues={content}
     typeSchema={typeSchema}
     onSave={values => {
       save(content.type, values.slug, values);
     }}
-  />);
+  />
+);
 
-Edit.getInitialProps = async ({query}) => {
+Edit.getInitialProps = async ({ query }) => {
   const content = await load(query.contentType, query.slug);
   const typeSchema = await getContentTypeMeta(query.contentType);
+
   return { content, typeSchema };
-}
+};
 
 export default Edit;
--- pages/admin/content/list.jsx
+++ pages/admin/content/list.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import Button from '@material-ui/core/Button'
-import {Form, Text } from 'informed';
-import {list} from '../../../core/client/api/content';
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import Link from 'next/link'
+import { list } from '../../../core/client/api/content';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Link from 'next/link';
 
-const ContentList = ({contentList, contentType}) => {
-  return (<List>
-    {
-      contentList.map(entry => <ListItem key={`list-item-${entry.slug}`}>
-          <Link
-            href={`/admin/content/edit?slug=${entry.slug}&contentType=${contentType}`}
-          >
+const ContentList = ({ contentList, contentType }) => {
+  return (
+    <List>
+      {contentList.map(entry => (
+        <ListItem key={`list-item-${entry.slug}`}>
+          <Link href={`/admin/content/edit?slug=${entry.slug}&contentType=${contentType}`}>
             {entry.slug}
           </Link>
         </ListItem>
-      )
-    }
-  </List>);
-}
+      ))}
+    </List>
+  );
+};
 
-ContentList.getInitialProps = async ({query}) => {
+ContentList.getInitialProps = async ({ query }) => {
   const contentList = await list(query.contentType);
   return {
     contentList,
     contentType: query.contentType,
   };
-}
+};
 
 export default ContentList;
