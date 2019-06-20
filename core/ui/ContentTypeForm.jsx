60d4b08328f5a08fa44db130d90219d6fc678cd0
Pavel Dubrouski <JoikerVik@gmail.com>
2019-06-20 11:06:38 +0300

    Add multiline-text field

--- core/ui/ContentTypeForm.jsx
+++ core/ui/ContentTypeForm.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import { Form } from 'informed';
 import Button from '@material-ui/core/Button';
 
-const ContentTypeForm = ({ typeSchema, initialValues = {}, onSave}) => {
-  console.log(typeSchema.fields)
-  return (<Form
-    initialValues={initialValues}
-    onSubmit={onSave}
-  >
-    <div>
-      {typeSchema.fields.map(field => {
-        return <field.Renderer
-          field={field.name}
-          label={field.name}
-        />;
-      })}
-      <Button type="submit">Save</Button>
-    </div>
-  </Form>);
-}
+const ContentTypeForm = ({ typeSchema, initialValues = {}, onSave }) => {
+  return (
+    <Form initialValues={initialValues} onSubmit={onSave}>
+      <div>
+        {typeSchema.fields.map((field, index) => {
+          return <field.Renderer key={index} field={field.name} label={field.name} />;
+        })}
+        <Button type="submit">Save</Button>
+      </div>
+    </Form>
+  );
+};
 
 export default ContentTypeForm;
