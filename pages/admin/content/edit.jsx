46b734800207b70c93b90516e4c8127b0c3a9918
Alexey Stasevich <alexey.stasevich@sunmait.com>
2019-06-25 02:13:49 -0700

    CRUD for content types

--- pages/admin/content/edit.jsx
+++ pages/admin/content/edit.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Router from 'next/router';
 import { load, save } from '../../../core/client/api/content';
 import { getContentTypeMeta } from '../../../core/entities/content-type';
 import ContentTypeForm from '../../../core/ui/ContentTypeForm';
@@ -8,7 +9,15 @@ const Edit = ({ content, typeSchema }) => (
     initialValues={content}
     typeSchema={typeSchema}
     onSave={values => {
-      save(content.type, values.slug, values);
+      values.slug = values.slug.trim();
+      save(content.type, content.slug, { type: content.type, ...values }).then(
+        response => {
+          Router.push('/admin/content/list?contentType=' + content.type);
+        },
+        err => {
+          console.error(err);
+        },
+      );
     }}
   />
 );
@@ -16,8 +25,9 @@ const Edit = ({ content, typeSchema }) => (
 Edit.getInitialProps = async ({ query }) => {
   const content = await load(query.contentType, query.slug);
   const typeSchema = await getContentTypeMeta(query.contentType);
+  const slug = query.slug;
 
-  return { content, typeSchema };
+  return { content, typeSchema, slug };
 };
 
 export default Edit;
--- server/content.js
+++ server/content.js
@@ -2,14 +2,56 @@ const fse = require('fs-extra');
 const fs = require('fs');
 const path = require('path');
 
+const fileExistError = new Error('This slug name is already taken');
+
 const save = (contentType, contentName, data, cb) => {
-  console.log(data);
+  const parsedData = JSON.parse(String(data));
   const filePath = path.resolve('_content', contentType, contentName + '.json');
-  fse.outputFile(filePath, data, cb);
+  const newFilePath = path.resolve('_content', contentType, parsedData.slug + '.json');
+
+  if (contentName === parsedData.slug) {
+    fse.outputFile(newFilePath, data, cb);
+  } else {
+    fs.stat(newFilePath, function(err, stat) {
+      if (err === null) {
+        throw fileExistError;
+      } else if (err.code === 'ENOENT') {
+        fs.rename(filePath, newFilePath, err => {
+          if (err) throw err;
+          fse.outputFile(newFilePath, data, cb);
+        });
+      } else {
+        console.error(err.code);
+        throw err;
+      }
+    });
+  }
 };
 
-const load = (contentType, contentSlug, cb) => {
-  const filePath = path.resolve('_content', contentType, contentSlug + '.json');
+const create = (contentType, contentName, data, cb) => {
+  const filePath = path.resolve('_content', contentType, contentName + '.json');
+  fs.stat(filePath, function(err, stat) {
+    if (err === null) {
+      throw fileExistError;
+    } else if (err.code === 'ENOENT') {
+      fs.appendFile(filePath, data, function(err) {
+        if (err) throw err;
+      });
+    } else {
+      throw err;
+    }
+  });
+};
+
+const deleteContent = (contentType, contentName, cb) => {
+  const filePath = path.resolve('_content', contentType, contentName + '.json');
+  fs.unlink(filePath, err => {
+    if (err) throw err;
+  });
+};
+
+const load = (contentType, contentName, cb) => {
+  const filePath = path.resolve('_content', contentType, contentName + '.json');
   fs.readFile(filePath, (err, data) => {
     cb(JSON.parse(String(data)));
   });
@@ -18,14 +60,14 @@ const load = (contentType, contentSlug, cb) => {
 const list = (type, cb) => {
   const contentDir = path.resolve('_content', type);
   readFiles(contentDir, cb);
-}
+};
 
-const listTypes = (cb) => {
+const listTypes = cb => {
   const contentDir = path.resolve('configs', 'content-types');
-  readFiles(contentDir, (types) => {
+  readFiles(contentDir, types => {
     cb(types.map(entry => entry.type));
   });
-}
+};
 
 function readFiles(dirname, cb) {
   fs.readdir(dirname, function(err, filenames) {
@@ -34,17 +76,19 @@ function readFiles(dirname, cb) {
       return;
     }
 
-    Promise.all(filenames.map(function(filename) {
-      return new Promise((resolve, reject) => {
-        fs.readFile(path.resolve(dirname, filename), 'utf-8', function(err, content) {
-          if (err) {
-            console.error(err);
-            reject();
-          }
-          resolve(JSON.parse(content))
+    Promise.all(
+      filenames.map(function(filename) {
+        return new Promise((resolve, reject) => {
+          fs.readFile(path.resolve(dirname, filename), 'utf-8', function(err, content) {
+            if (err) {
+              console.error(err);
+              reject();
+            }
+            resolve(JSON.parse(content));
+          });
         });
-      });
-    })).then(files => {
+      }),
+    ).then(files => {
       cb(files);
     });
   });
@@ -55,4 +99,6 @@ module.exports = {
   listTypes,
   save,
   load,
+  create,
+  deleteContent,
 };
